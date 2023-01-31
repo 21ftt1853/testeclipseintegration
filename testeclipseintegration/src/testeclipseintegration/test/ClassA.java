e1ba9c8e5b6d9377f082534e543869a5256a34e0
Hazwani Afiqah <21FTT1853@student.pb.edu.bn>
2023-01-31 15:28:19 +0800

    Trying basic OOP

---
+++ testeclipseintegration/src/testeclipseintegration/test/ClassA.java
@@ -0,0 +1,5 @@
+package testeclipseintegration.test;
+
+public class ClassA {
+
+}
--- testeclipseintegration/src/testeclipseintegration/test/test.java
+++ testeclipseintegration/src/testeclipseintegration/test/test.java
@@ -3,5 +3,7 @@ package testeclipseintegration.test;
 public class test {
 public static void main(String[] args) {
 	System.out.println("GitHub");
+	ClassA classA = new ClassA("ABCDEF");
+	System.out.println(classA.getAbc());
 }
 }
